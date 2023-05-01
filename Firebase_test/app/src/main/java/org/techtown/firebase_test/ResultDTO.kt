53a0a9ce05ffe458980ee1371a4735d73f9f9352
Juhui Heo <82089499+JuHuiHeo@users.noreply.github.com>
2023-05-01 14:52:26 +0900

    Delete Firebase_test directory

--- Firebase_test/app/src/main/java/org/techtown/firebase_test/ResultDTO.kt
+++
@@ -1,9 +0,0 @@
-package org.techtown.firebase_test
-
-
-data class ResultDTO (
-    var uid :String? = null,
-    var textOne : String? = null,
-    var textTwo : String? = null,
-    var timestamp : Long? = null
-)
